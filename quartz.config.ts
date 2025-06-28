02d3798c077498a97fa082a0cb109a0ba4a7b712
Andrew Chuah <warlordpluto@gmail.com>
2025-06-28 03:53:56 -0400

    added back broken config opt

--- quartz.config.ts
+++ quartz.config.ts
@@ -52,6 +52,7 @@ const config: QuartzConfig = {
         },
       },
     },
+    generateSocialImages: false,
   },
   plugins: {
     transformers: [
@@ -90,7 +91,9 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({
+        colorScheme: "darkMode",
+      }),
     ],
   },
 }
